d3bab57d247760060eb72de037651ea5c92674be
Benjamin Likita <benjaminlikita3@gmail.com>
2025-03-23 18:13:21 +0100

    update use-websocket ook

--- src/hooks/use-websocket.ts
+++ src/hooks/use-websocket.ts
@@ -3,7 +3,8 @@
 import { io } from 'socket.io-client'
 
 
-export const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL)
+// export const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL)
+export const socket = io()
 
 export const requestPrivateCallAccess = (data: {username: string, roomId: string, userId: string}) => {
   socket?.emit('requestPrivateCallAccess', data)
